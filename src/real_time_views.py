b3090c91d128178ffb2eddc284ca63a56a7b9f02
hila <hilapazh@gmail.com>
2022-11-25 17:17:28 +0200

    fix: static processing time for event based messages

--- src/real_time_views.py
+++ src/real_time_views.py
@@ -1,6 +1,7 @@
 import os
 import logging
 from datetime import datetime
+from functools import partial
 
 import boto3
 import pandas as pd
@@ -25,7 +26,10 @@ def save_data_to_timestream(df: pd.DataFrame) -> None:
                                aws_secret_access_key=os.getenv("AWS_SECRET"))
     logger.info("successfully connected to timestream-write client")
 
-    records = df.apply(convert_series_to_dict, axis=1)
+    date = str(int(datetime.timestamp(datetime.now()) * 1000))
+    convert_series_now = partial(convert_series_to_dict, date=date)
+
+    records = df.apply(convert_series_now, axis=1)
 
     time_stream.write_records(DatabaseName='spotify',
                               TableName='albums',
@@ -35,7 +39,7 @@ def save_data_to_timestream(df: pd.DataFrame) -> None:
                 extra={"database": "spotify", "table": "albums", "batch_size": len(records)})
 
 
-def convert_series_to_dict(row: pd.Series) -> dict:
+def convert_series_to_dict(row: pd.Series, date: str) -> dict:
     """
 
     Args:
@@ -43,12 +47,12 @@ def convert_series_to_dict(row: pd.Series) -> dict:
             track_popularity
             artist
             album
+        date: str representation of processing time
 
     Returns: a record to match AWS TimeStream format
 
     """
-    time = str(int(datetime.timestamp(datetime.now()) * 1000))
-    record = {"Time": time,
+    record = {"Time": date,
               "MeasureValue": str(row['track_popularity']),
               "MeasureValueType": "DOUBLE",
               "MeasureName": "popularity",
--- src/visualization.py
+++ src/visualization.py
@@ -1,6 +1,8 @@
 import os
 import uuid
 import logging
+from datetime import datetime
+from functools import partial
 
 import pandas as pd
 from elasticsearch import Elasticsearch
@@ -22,27 +24,31 @@ def save_data_to_es(df: pd.DataFrame) -> None:
     """
     es = Elasticsearch(cloud_id=os.environ.get('ES_CLOUD_ID'), basic_auth=('elastic', os.environ.get('ES_PASS')))
 
-    docs = df.apply(get_document_from_row, axis=1)
+    date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    get_doc_with_time = partial(get_document_from_row, date=date)
+
+    docs = df.apply(get_doc_with_time, axis=1)
     [es.index(index="artists", document=doc) for doc in docs]
 
     logger.info("successfully placed documents in elastic search",
                 extra={"records": len(docs), "index": "artists"})
 
 
-def get_document_from_row(row: pd.Series) -> dict:
+def get_document_from_row(row: pd.Series, date: str) -> dict:
     """
     Generate an ElasticSearch formatted document
     Args:
         row: pd.Series with relevant information for the doc
+        date: str representation for processing time
 
     Returns: Dictionary with keys to compliment artist table mapping
 
     """
     document = {
         "artistId": str(uuid.uuid4()),
-        "eventTime": row['date'].strftime("%Y-%m-%d %H:%M:%S"),
+        "eventTime": date,
         "artist": row['artist'],
-        "popularity": row['artist_popularity'],
+        "popularity": row['track_popularity'],
         "followers": row['artist_followers']
     }
     return document
