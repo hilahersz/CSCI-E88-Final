e3cb6091cff3cf00914ba68abef3d8be894bd096
hila <hilapazh@gmail.com>
2022-11-25 11:10:17 +0200

    feat: batch processing

--- src/batch_processing.py
+++ src/batch_processing.py
@@ -1,4 +1,6 @@
+from typing import List
 from datetime import datetime
+from tempfile import TemporaryDirectory
 
 import dask.dataframe as dd
 import pandas as pd
@@ -6,11 +8,24 @@ import pandas as pd
 GROUP_KEY = ["artist", "album"]
 
 
-def transform_dask_to_time_stream(date: datetime.date) -> pd.DataFrame:
+def aggregate_data(
+        agg_func: callable,
+        dfs: List[pd.DataFrame]
+):
+    with TemporaryDirectory() as d:
+        for df in dfs:
+            artist_key = df['artist'][0]
+            file_path = f"{d}/{artist_key}.csv"
+            df.to_csv(file_path, index=False)
+        files_path = f"{d}/*.csv"
+        agg_func(files_path)
+
+
+def transform_dask_to_time_stream(files_path: str) -> pd.DataFrame:
     """
     Batch processing function for AWS TimeStream to include aggregations by the album level.
     Args:
-        date: The date to index that transformations should happen on
+        files_path: The location of csv files to be processed
 
     Returns: a single dataframe for all artists with the following columns:
         artist: name of artist
@@ -18,10 +33,9 @@ def transform_dask_to_time_stream(date: datetime.date) -> pd.DataFrame:
         track_popularity: the mean popularity
     """
 
-    file_path = f"data/{date}_*.csv"
-    ddf = dd.read_csv(file_path)
+    ddf = dd.read_csv(files_path)
 
-    album_info = ddf\
+    album_info = ddf \
         .groupby(GROUP_KEY) \
         .track_popularity \
         .mean() \
@@ -30,27 +44,23 @@ def transform_dask_to_time_stream(date: datetime.date) -> pd.DataFrame:
     return album_info.reset_index()
 
 
-def transform_dask_to_es(date):
+def transform_dask_to_es(files_path: str) -> pd.DataFrame:
     """
     Batch processing function for ES to include aggregations by the artist level.
 
     Args:
-        date: The date to index that transformations should happen on
+        files_path: The location of csv files to be processed
 
     Returns: a single dataframe for all artists with the following columns:
         artist: name of artist
         track_popularity: average popularity across different tracks
         artist_followers: count of followers of the artist
     """
-
-    file_path = f"data/{date}_*.csv"
-    ddf = dd.read_csv(file_path)
+    ddf = dd.read_csv(files_path)
 
     mean_popularity = ddf \
         .groupby("artist") \
-        .aggregate(arg={
-            "track_popularity": "mean",
-            "artist_followers": "max",
-        }
-        ).compute()
+        .aggregate(arg={"track_popularity": "mean", "artist_followers": "max"}
+                   ).compute()
+
     return mean_popularity.reset_index()
